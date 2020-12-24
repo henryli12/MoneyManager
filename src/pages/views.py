fe85dd53541ae2359fbbe98b00eee7affd0a3518
henryli12 <xiaofengli2016@gmail.com>
2020-12-24 15:09:31 -0500

    set up static folder, and working on html

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from transactions.models import Transaction
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return render(request, "index.html", {})
+    transactions = Transaction.objects.all()
+    context = {
+        'transactions': transactions
+    }
+    return render(request, "index.html", context)
 
 def edit_view(request, *args, **kwargs):
     return render(request, "edit.html", {})
--- src/transactions/models.py
+++ src/transactions/models.py
@@ -7,4 +7,5 @@ class Transaction(models.Model):
     description = models.TextField(blank=True, null=True)
     amount = models.DecimalField(max_digits=6, decimal_places=2)
     type = models.CharField(max_length=1)
-    date = models.DateTimeField(auto_now=True)
+    date = models.DateField()
+
