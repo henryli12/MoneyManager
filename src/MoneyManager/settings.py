da0436c9b5188a82daa332d77e6aca2f6dc113a1
henryli12 <xiaofengli2016@gmail.com>
2020-12-23 21:57:33 -0500

    added dotenv to hide secret-key

--- src/MoneyManager/settings.py
+++ src/MoneyManager/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w1_&-w1qxgdmw0s_7$idtj4o8um@kf-5n)jw$1r1m9a7ww$-ji'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
