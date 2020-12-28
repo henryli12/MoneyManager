dfbdf939c0e07b649d931022016042c7fbf61195
henryli12 <xiaofengli2016@gmail.com>
2020-12-27 20:28:52 -0500

    small ui change

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -3,20 +3,24 @@ $( document ).ready(function() {
     addTransactionCard(initTransactions);
     calendar.render();
 
-    $('#add_date').attr("value", formatDate(new Date()));
+    $('#add_date').attr("value", formatDate(new Date(), "date"));
+    $('#month_display').attr("value", formatDate(new Date(), "month"));
     document.getElementById("add").onclick = function() { 
         $('#add_modal').modal();
    }
 
 });
 
-function formatDate(d) {
+function formatDate(d, type) {
     let month = '' + (d.getMonth() + 1);
     let day = '' + d.getDate();
     let year = d.getFullYear();
 
     if (month.length < 2) 
         month = '0' + month;
+    if (type === "month"){
+        return [year, month].join('-');
+    }
     if (day.length < 2) 
         day = '0' + day;
 
